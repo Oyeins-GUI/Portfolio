99514a2404c8dad646819358a8fca9a0d51cabe0
Oyeinbomo <oyeinx7@gmail.com>
2025-01-11 10:00:54 +0100

    Update pfp import

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,14 @@
-import pfp2 from "../assets/pfp2.webp";
+import pfp from "../assets/pfp1.webp";
 
 export default function Profile({ type }: { type: "sm" | "lg" }) {
    if (type === "sm")
       return (
          <div className="w-24 h-24 rounded-full overflow-hidden">
             <img
-               src={pfp2}
+               src={pfp}
                alt="profile"
                className="border-2 border-white/20 w-full block object-cover"
-               srcSet={pfp2}
+               srcSet={pfp}
                loading="lazy"
                decoding="async"
                fetchPriority="high"
@@ -19,10 +19,10 @@ export default function Profile({ type }: { type: "sm" | "lg" }) {
    return (
       <div className="w-48 h-48 rounded-full overflow-hidden">
          <img
-            src={pfp2}
+            src={pfp}
             alt="profile"
             className="border-4 border-white/20 w-full block object-cover"
-            srcSet={pfp2}
+            srcSet={pfp}
             loading="lazy"
             decoding="async"
             fetchPriority="high"
